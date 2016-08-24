94c01ffc4140808f9ca8a6309c36bb808bea3565
Joshua Read <joshua.gray.read@gmail.com>
2016-08-24 07:27:34 +0200

    trying to get this onto staging and live server again

--- wp-load.php
+++ wp-load.php
@@ -18,7 +18,7 @@
  * @package WordPress
  */
 
-/** Define ABSPATH as this file's directory */
+/** Define ABSPATH as this file's directory--- */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
